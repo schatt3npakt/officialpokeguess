a9f3608b7cb6308a876355c5e540c500c38a1536
Luca La Furia <dev@bitheart.de>
2022-11-04 19:29:01 +0100

    offload google fonts

--- src/App.vue
+++ src/App.vue
@@ -180,21 +180,22 @@ html {
 
 /* nunito-sans-regular - latin */
 @font-face {
-  font-family: 'Nunito Sans';
+  font-family: 'Nunito';
   font-style: normal;
   font-weight: 400;
   src: local(''),
-       url('./assets/fonts/nunito-sans-v12-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
-       url('./assets/fonts/nunito-sans-v12-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+       url('./assets/nunito-sans-v12-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('./assets/nunito-sans-v12-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
 }
+
 /* nunito-sans-900 - latin */
 @font-face {
-  font-family: 'Nunito Sans';
+  font-family: 'Nunito';
   font-style: normal;
   font-weight: 900;
   src: local(''),
-       url('./assets/fonts/nunito-sans-v12-latin-900.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
-       url('./assets/fonts/nunito-sans-v12-latin-900.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+       url('./assets/nunito-sans-v12-latin-900.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('./assets/nunito-sans-v12-latin-900.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
 }
 
 * {
